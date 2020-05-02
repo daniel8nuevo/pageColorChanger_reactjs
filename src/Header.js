a8f738ee4d598abbd08c2f2626aece7f5be1d4ab
daniel8nuevo <nuevo8vida@gmail.com>
2020-05-02 23:18:14 +0300

    create header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '.header.css';
+
+export default class Header extends React.Component {
+    render(){
+        return(
+            <h1 className='header'>
+                Try to change color
+            </h1>
+        )
+    }
+}
